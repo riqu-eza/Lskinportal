f77710722ff869c2e6766de577a62ba1dceee1a4
offline <vickymuthunga@gmail.com>
2024-12-04 09:49:17 +0300

    for deployement

--- api/index.js
+++ api/index.js
@@ -38,7 +38,7 @@ app.use(
 );
 
 app.use(cors({
-  origin: 'https://cosytwobedroominthika.online',
+  origin: 'lskinessentials.com',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credintials:true,
 }));
