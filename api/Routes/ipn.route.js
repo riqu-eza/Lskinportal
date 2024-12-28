a51dcde58d412c10ef4a4e028de7ad65f9b5d6d2
offline <vickymuthunga@gmail.com>
2024-12-28 18:06:50 +0300

    well of it

--- api/Routes/ipn.route.js
+++ api/Routes/ipn.route.js
@@ -2,5 +2,8 @@ import express from "express";
 import { ipnreat } from "../controllers/ipn.controller.js";
 
 const router = express.Router();
+
 router.post("/callbackurl", ipnreat);
+
+
 export default router;
